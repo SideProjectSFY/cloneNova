190a25c37cea7f8c59cc1d1c626be403117c2fb0
Kim, Ji-Hea <bictiger3@gmail.com>
2025-10-20 00:03:13 +0900

    Chore : Gradle 프로젝트 환경설정

---
+++ src/main/java/com/ssafy/cloneNova/CloneNovaApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.cloneNova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneNovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneNovaApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/ssafy/cloneNova/CloneNovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.cloneNova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneNovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
