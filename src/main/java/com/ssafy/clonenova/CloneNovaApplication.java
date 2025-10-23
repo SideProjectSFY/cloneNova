d131e949a4dec9f4262d405e57472d1551f8d732
Jihea <bictiger3@gmail.com>
2025-10-23 22:42:13 +0900

    Chore : Swagger Docs 세팅

--- src/main/java/com/ssafy/clonenova/CloneNovaApplication.java
+++ src/main/java/com/ssafy/clonenova/CloneNovaApplication.java
@@ -3,7 +3,8 @@ package com.ssafy.clonenova;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class CloneNovaApplication {
 
 	public static void main(String[] args) {
