0da948e3e33da21505f3281701cb1b6697bd5695
Lee Danhye <dan0628@naver.com>
2025-10-28 07:16:18 +0900

    PortOneProperties 추가

--- src/main/java/com/ssafy/clonenova/CloneNovaApplication.java
+++ src/main/java/com/ssafy/clonenova/CloneNovaApplication.java
@@ -2,8 +2,11 @@ package com.ssafy.clonenova;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.ssafy.clonenova.config.PortOneProperties;
 
+@EnableConfigurationProperties(PortOneProperties.class)
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class CloneNovaApplication {
 
